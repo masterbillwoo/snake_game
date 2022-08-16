d548d91afeaa20fc9d48df8ca3e91d5192301790
masterbillwoo <masterbillwoo@gmail.com>
2022-08-16 14:22:35 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+from turtle import Screen, Turtle
+
+screen = Screen()
+screen.setup(600,600)
+screen.bgcolor("black")
+screen.title("My Snake Game")
+
+
+
+screen.exitonclick()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
