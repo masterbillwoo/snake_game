639ba73d8d8b9333aec2024b7f9a05ad6361b824
masterbillwoo <masterbillwoo@gmail.com>
2022-08-16 23:13:49 -0400

    snek

--- main.py
+++ main.py
@@ -2,16 +2,36 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-from turtle import Screen, Turtle
+import time
+from turtle import Screen, Turtle, forward
+from snake import Snake
 
 screen = Screen()
-screen.setup(600,600)
+screen.setup(600, 600)
 screen.bgcolor("black")
 screen.title("My Snake Game")
+screen.tracer(0)
+
+snake = Snake()
+
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    #snake.down()
+    snake.move()
 
 
 
-screen.exitonclick()
+
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
